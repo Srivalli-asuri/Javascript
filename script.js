0ebc007b1ee0678a36c82cec22c664fb19b3c436
Srivalli-asuri <srivalliasuri18@gmail.com>
2025-08-24 11:56:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
-let header=document.createElement("h1")
-header.textContent="Coin Tosser"
-header.id="h1"
-document.body.appendChild(header)
+document.getElementById("container").style.backgroundColor="white"
+document.getElementById("container").style.fontSize="5px"
+let randomNumber =Math.random()*100
+randomNumber=Math.ceil(randomNumber)
+function Guessthenumber(){
+    console.log("function is triggered...")
 
-let image=document.createElement("img")
-image.src="https://tse3.mm.bing.net/th/id/OIP.u313lXWkrjcT6YnqJLNYVwHaEK?pid=Api&P=0&h=180"
-image.id="img"
-document.body.appendChild(image)
+    let currentValue=document.getElementById("user_input").value
+   
+    currentValue=parseInt(currentValue)
 
-const headsImg = "https://i.ytimg.com/vi/6FLBpQQP96U/hqdefault.jpg";
-const tailsImg = "https://cdn.textstudio.com/output/sample/normal/3/8/2/6/tail-logo-73-16283.png";
-
-
-let btn=document.createElement("button")
-btn.textContent="Click me"
-btn.id="btn"
-let result=document.createElement("p")
-result.id="result"
-document.body.appendChild(document.createElement("br"))
-document.body.appendChild(btn)
-
- btn.onclick=function tosscoin(){
-    let randomnum=Math.ceil(Math.random() *2)
-    if(randomnum==1){
-        image.src=headsImg;
-        result.textContent="Head"
-        
+    if(currentValue==randomNumber){
+        document.getElementById("message").textContent="Your guessing number is correct!"
+    }else if(currentValue>randomNumber){
+         document.getElementById("message").textContent="Your guessing number is Too high Try Again!"
+    }else if(currentValue<randomNumber){
+          document.getElementById("message").textContent="Your guessing number is Too low Try Again!"
     }else{
-        image.src=tailsImg
-        result.textContent="Tail"
-        
+        document.getElementById("message").textContent="please eneter a valid input"
     }
+
 }
-tosscoin()
-
-
